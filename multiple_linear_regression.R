182f8e8579d31dc928045db38424e4f03c365283
Lucas Connors <lucas.revolutiontech@gmail.com>
2016-12-11 15:14:31 -0800

    Use data-preprocessing template
    to prepare multiple linear regression in R

---
+++ multiple_linear_regression.R
@@ -0,0 +1,18 @@
+# Multiple Linear Regression
+
+# Importing the dataset
+dataset = read.csv('50_Startups.csv')
+
+# Encoding categorical data
+dataset$State = factor(
+  dataset$State,
+  levels=c('New York', 'California', 'Florida'),
+  labels=c(1, 2, 3)
+)
+
+# Splitting the dataset into the training and test datasets
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Profit, SplitRatio=0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
